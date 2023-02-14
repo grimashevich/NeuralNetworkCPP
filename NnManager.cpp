5effed543ae1c2a22495bb297cd0d771f6300435
Elyas Clown <eclown@student.21-school.ru>
2023-02-14 20:00:57 +0300

    NnManager in progress (just save)

--- NnManager.cpp
+++ NnManager.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "NnManager.h"
+
+NnManager::NnManager()
+{
+
+}
+
+double NnManager::getPrecision() const
+{
+	return precision;
+}
+
+double NnManager::getRecall() const
+{
+	return recall;
+}
+
+double NnManager::getFMeasure() const
+{
+	return fMeasure;
+}
+
+int NnManager::getInputSizeNn() const
+{
+	return inputSizeNN;
+}
+
+int NnManager::getOutputSizeNn() const
+{
+	return outputSizeNN;
+}
+
--- NnManager.h
+++ NnManager.h
@@ -3,11 +3,31 @@
 
 #include "NnBase.h"
 #include "NN.h"
+#include "TrainingSet.h"
+#include "StopWatch.h"
+
 class NnManager
 {
-
 private:
+	int inputSizeNN = 0;
+	int outputSizeNN = 0;
+	NnBase *nn = nullptr;
+	TrainingSet *trainingSet = nullptr;
+	TrainingSet *testSet = nullptr;
+	double precision = 0;
+	double recall = 0;
+	double fMeasure = 0;
 
+public:
+	NnManager();
+	void LoadMatrixNN();
+	void LoadGraphNN();
+
+	[[nodiscard]] double getPrecision() const;
+	[[nodiscard]] double getRecall() const;
+	[[nodiscard]] double getFMeasure() const;
+	[[nodiscard]] int getInputSizeNn() const;
+	[[nodiscard]] int getOutputSizeNn() const;
 };
 
 
